848ba6115fcbe0426a3abaa3991aa21997ff58fe
jolin <jolin.xia1@gmail.com>
2025-04-13 10:52:54 -0600

    fix return error

--- models/CMN_model.py
+++ models/CMN_model.py
@@ -42,27 +42,31 @@ class CMNModel(nn.Module):
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
 
-    def forward_iu_xray(self, images, targets=None, mode='train', update_opts={}):
+    def forward_iu_xray(self, images, targets=None, mode="train", update_opts={}):
         att_feats_0, fc_feats_0 = self.visual_extractor(images[:, 0])
         att_feats_1, fc_feats_1 = self.visual_extractor(images[:, 1])
         fc_feats = torch.cat((fc_feats_0, fc_feats_1), dim=1)
         att_feats = torch.cat((att_feats_0, att_feats_1), dim=1)
-        if mode == 'train':
-            output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
-            return output
-        elif mode == 'sample':
-            output, output_probs = self.encoder_decoder(fc_feats, att_feats, mode='sample', update_opts=update_opts)
+        if mode == "train":
+            output = self.encoder_decoder(fc_feats, att_feats, targets, mode="forward")
+            return output, None
+        elif mode == "sample":
+            output, output_probs = self.encoder_decoder(
+                fc_feats, att_feats, mode="sample", update_opts=update_opts
+            )
             return output, output_probs
         else:
             raise ValueError
 
-    def forward_mimic_cxr(self, images, targets=None, mode='train', update_opts={}):
+    def forward_mimic_cxr(self, images, targets=None, mode="train", update_opts={}):
         att_feats, fc_feats = self.visual_extractor(images)
-        if mode == 'train':
-            output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
-            return output
-        elif mode == 'sample':
-            output, output_probs = self.encoder_decoder(fc_feats, att_feats, mode='sample', update_opts=update_opts)
+        if mode == "train":
+            output = self.encoder_decoder(fc_feats, att_feats, targets, mode="forward")
+            return output, None
+        elif mode == "sample":
+            output, output_probs = self.encoder_decoder(
+                fc_feats, att_feats, mode="sample", update_opts=update_opts
+            )
             return output, output_probs
         else:
             raise ValueError
