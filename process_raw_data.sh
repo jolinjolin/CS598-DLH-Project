54a891d4ac2830878e73552ef1adc40d83c236d2
jolin <jolin.xia1@gmail.com>
2025-04-23 22:42:59 -0600

    add scripts for processing raw data

---
+++ process_raw_data.sh
@@ -0,0 +1,3 @@
+python tasks/process_raw_data.py \
+    --xml_folder data/iu_xray/ecgen-radiology/ \
+    --output_json_path data/iu_xray/annotation.json \
---
+++ tasks/process_raw_data.py
@@ -0,0 +1,103 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import json
+import xml.etree.ElementTree as ET
+import argparse
+import random
+
+max_findings_length = 0
+max_impression_length = 0
+max_tot_length = 0
+
+def parse_agrs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--xml_folder', type=str, help='the folder containing the XML files.')
+    parser.add_argument('--output_json_path', type=str, default='output.json', help='the path to the output JSON file.')
+
+    args = parser.parse_args()
+
+    return args
+
+def extract_report_from_xml(xml_file, split):
+    global max_findings_length, max_impression_length, max_tot_length
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    findings = ""
+    impression = ""
+    image_ids = []
+
+    for elem in root.iter():
+        if elem.tag == "AbstractText" and "Label" in elem.attrib:
+            if elem.attrib["Label"] == "FINDINGS":
+                findings = elem.text.strip() if elem.text else ""
+            elif elem.attrib["Label"] == "IMPRESSION":
+                impression = elem.text.strip() if elem.text else ""
+        elif elem.tag == "parentImage" and "id" in elem.attrib:
+            image_id = elem.attrib["id"]
+            image_ids.append(image_id)
+
+    if len(image_ids) < 2:
+        # print(xml_file, image_ids)
+        return None
+    if len(impression) > 60:
+    # if len(findings) > 60:
+        # print(xml_file)
+        return None
+    
+    tot_len = len(findings) + len(impression)
+    max_findings_length = max(max_findings_length, len(findings))
+    max_impression_length = max(max_impression_length, len(impression)) 
+    max_tot_length = max(max_tot_length, tot_len)
+
+
+    # report_text = findings if findings else impression
+    report_text = impression if impression else findings
+    study_id = image_ids[0].split("-")[0]
+    png1 = image_ids[0].split("-")[1]
+    png2 = image_ids[1].split("-")[1]
+    return {
+        "id": study_id,
+        "report": report_text,
+        "image_path": [f"{study_id}/{png1}.png", f"{study_id}/{png2}.png"],
+        "split": split
+    }
+
+def process_all_xml(xml_folder):
+    dataset = {"train": [], "val": [], "test": []}
+    files = os.listdir(xml_folder)
+    random.shuffle(files)  # Randomize the order
+    total = len(files)
+    train_cutoff = int(0.7 * total)
+    val_cutoff = int(0.9 * total)  # 70% + 20% = 90%
+
+    split = "train"
+    for i, filename in enumerate(files):
+        if i < train_cutoff:
+            split = "train"
+        elif i < val_cutoff:
+            split = "val"
+        else:
+            split = "test"
+        
+        if filename.endswith(".xml"):
+            file_path = os.path.join(xml_folder, filename)
+            result = extract_report_from_xml(file_path, split)
+            if result:
+                dataset[split].append(result)
+
+    print(f"Max findings length: {max_findings_length}") 
+    print(f"Max impression length: {max_impression_length}")
+    print(f"Max total length: {max_tot_length}")
+    tot_report = len(dataset['train']) + len(dataset['val']) + len(dataset['test'])
+
+    return dataset, tot_report
+
+if __name__ == "__main__":
+    args = parse_agrs()
+    result, tot_report = process_all_xml(args.xml_folder)
+    with open(args.output_json_path, "w") as f:
+        json.dump(result, f, indent=2)
+    print(f"Saved {tot_report} reports to {args.output_json_path}")
