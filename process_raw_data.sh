09d8f7a978194861f9516880797495e1935f26bb
jolin <jolin.xia1@gmail.com>
2025-04-26 00:15:58 -0600

    update scripts for raw data

--- process_raw_data.sh
+++ process_raw_data.sh
@@ -1,3 +1,3 @@
 python tasks/process_raw_data.py \
-    --xml_folder data/iu_xray/ecgen-radiology/ \
-    --output_json_path data/iu_xray/annotation.json \
+    --xml_folder data/iu_xray_raw/ecgen-radiology/ \
+    --output_json_path data/iu_xray_raw/annotation.json \
--- tasks/token_sets.py
+++ tasks/token_sets.py
@@ -4,8 +4,8 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import spacy
-from spacy.tokens import Doc
 from utils.tokenizers import Tokenizer
+import json
 
 
 def parse_agrs():
@@ -13,15 +13,9 @@ def parse_agrs():
 
     # Data input settings
     parser.add_argument(
-        "--image_dir",
-        type=str,
-        default="data/iu_xray/images/",
-        help="the path to the directory containing the data.",
-    )
-    parser.add_argument(
         "--ann_path",
         type=str,
-        default="data/iu_xray/annotation.json",
+        default="data/iu_xray_raw/annotation.json",
         help="the path to the directory containing the data.",
     )
     parser.add_argument('--dataset_name', type=str, default='iu_xray', choices=['iu_xray', 'mimic_cxr'],
@@ -31,66 +25,85 @@ def parse_agrs():
     args = parser.parse_args()
 
     return args
+                
 
 def get_sorted_tokens(tokenizer):
-    total_tokens = []
-    report_length, tot_report = 0, 0
-    for example in tokenizer.ann['train']:
-        tokens = tokenizer.clean_report(example['report']).split()
-        total_tokens.extend(tokens)
-        report_length += len(tokens)
-        tot_report += 1
-    
-    # for example in tokenizer.ann['val']:
+    # total_tokens = []
+    # report_length, tot_report = 0, 0
+    # for example in tokenizer.ann['train']:
     #     tokens = tokenizer.clean_report(example['report']).split()
     #     total_tokens.extend(tokens)
     #     report_length += len(tokens)
     #     tot_report += 1
     
-    # for example in tokenizer.ann['test']:
-    #     tokens = tokenizer.clean_report(example['report']).split()
-    #     total_tokens.extend(tokens)
-    #     report_length += len(tokens)
-    #     tot_report += 1
-
-    counter = Counter(total_tokens)
+    # # for example in tokenizer.ann['val']:
+    # #     tokens = tokenizer.clean_report(example['report']).split()
+    # #     total_tokens.extend(tokens)
+    # #     report_length += len(tokens)
+    # #     tot_report += 1
     
-    vocab_freqs = {tok: freq for tok, freq in counter.items() if freq >= tokenizer.threshold}
-    vocab_freqs['<unk>'] = 1e-5  # Give <unk> a tiny freq to keep it
+    # # for example in tokenizer.ann['test']:
+    # #     tokens = tokenizer.clean_report(example['report']).split()
+    # #     total_tokens.extend(tokens)
+    # #     report_length += len(tokens)
+    # #     tot_report += 1
 
-    sorted_tokens = sorted(vocab_freqs.items(), key=lambda x: x[1])
+    # counter = Counter(total_tokens)
+    
+    # vocab_freqs = {tok: freq for tok, freq in counter.items() if freq >= tokenizer.threshold}
+    # # vocab_freqs['<unk>'] = 1e-5  # Give <unk> a tiny freq to keep it
+    # # vocab_freqs = {tok: freq for tok, freq in counter.items()}
 
-    print(f"Avg report length: {report_length / tot_report}")
+    # sorted_tokens = sorted(vocab_freqs.items(), key=lambda x: x[1])
+
+    # print(f"Avg report length: {report_length / tot_report}")
+    # print(f"Vocab size: {len(sorted_tokens)}")
+
+    sorted_tokens = [w for w in tokenizer.token2idx][:-2]
     print(f"Vocab size: {len(sorted_tokens)}")
     return sorted_tokens
 
+def get_report_text(file_path):
+    with open(file_path, 'r') as f:
+        json_data = json.load(f)
+    for split in ["train","val", "test"]:
+        for example in json_data[split]:
+            if 'report' in example:
+                yield example['report']
 
-def extract_disease_entities(text):
-    nlp = spacy.load("en_ner_bc5cdr_md")
+def extract_disease_entities(nlp, text):
+    medical_terms = set()
     doc = nlp(text)
-    return len(doc.ents)
+    for ent in doc.ents:
+        if ent.label_ == "DISEASE":
+            for token in ent.text.split():
+                medical_terms.add(token)
+    return medical_terms
 
 
-def split_tokens(sorted_tokens, num_splits=5):
-    # Greedy frequency-balanced binning
-    splits = [[] for _ in range(num_splits)]
-    split_freqs = [0] * num_splits
-
-    for token, freq in sorted_tokens:
-        min_index = split_freqs.index(min(split_freqs))
-        splits[min_index].append(token)
-        split_freqs[min_index] += freq
-
+def split_tokens(sorted_tokens, medical_terms, num_splits=5):
+    # # Greedy frequency-balanced binning
     # splits = [[] for _ in range(num_splits)]
-    # num_tokens_per_split = len(sorted_tokens) // num_splits
-    # for i in range(num_splits):
-    #     start_index = i * num_tokens_per_split
-    #     end_index = (i + 1) * num_tokens_per_split if i != num_splits - 1 else len(sorted_tokens)
-    #     splits[i] = [token for token,_ in sorted_tokens[start_index:end_index]]
+    # split_freqs = [0] * num_splits
+
+    # for token, freq in sorted_tokens:
+    #     min_index = split_freqs.index(min(split_freqs))
+    #     splits[min_index].append(token)
+    #     split_freqs[min_index] += freq
+
+    splits = [[] for _ in range(num_splits)]
+    num_tokens_per_split = len(sorted_tokens) // num_splits
+    for i in range(num_splits):
+        start_index = i * num_tokens_per_split
+        end_index = (i + 1) * num_tokens_per_split if i != num_splits - 1 else len(sorted_tokens)
+        splits[i] = [token for token in sorted_tokens[start_index:end_index]]
 
     ratios = []
     for i, split in enumerate(splits):
-        num_medical = extract_disease_entities(" ".join(split))
+        num_medical = 0
+        for token in split:
+            if token in medical_terms:
+                num_medical += 1
         print(f"Split {i+1}:")
         print(f"Num of medical terms: {num_medical}")
         print(f"Num of all terms: {len(split)}")
@@ -99,9 +112,20 @@ def split_tokens(sorted_tokens, num_splits=5):
 
 def main():
     args = parse_agrs()
-    tokenizer = Tokenizer(args)
+    tot_num_tokens = 0
+    reports_txt= get_report_text(args.ann_path)
+    medical_terms_all = set()
+    nlp = spacy.load("en_ner_bc5cdr_md")
+    for report in reports_txt:
+        tot_num_tokens += len(report.split())
+        medical_terms = extract_disease_entities(nlp, report)
+        medical_terms_all.update(medical_terms)
+    print(f"Tot num of medical terms: {len(medical_terms_all)}")
+    print(f"Tot num of tokens: {tot_num_tokens}")
+
+    tokenizer = Tokenizer(args) 
     sorted_tokens = get_sorted_tokens(tokenizer)
-    split_tokens(sorted_tokens, num_splits=5)
+    split_tokens(sorted_tokens, medical_terms_all, num_splits=5)
 
     
 if __name__ == "__main__":
